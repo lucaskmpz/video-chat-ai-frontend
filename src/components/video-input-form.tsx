9b9288bb9e8e5c8bf6f257053a40f9115c02cb25
Lucas <lucasclmaia0@gmail.com>
2023-09-15 22:55:35 -0300

    updating values to textarea along completion and updating props

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -18,7 +18,11 @@ const statusMessages = {
     success: "Success!",
 }
 
-export function VideoInputForm() {
+interface VideoInputFormProps {
+    onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
     const [videoFile, setVideoFile] = useState<File | null>(null)
     const [status, setStatus] = useState<Status>("waiting")
 
@@ -106,6 +110,8 @@ export function VideoInputForm() {
         })
 
         setStatus("success")
+
+        props.onVideoUploaded(videoId)
     }
 
     const previewURL = useMemo(() => {
